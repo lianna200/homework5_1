77289c644cb4cf730823376cd2315e4814886c9f
lianna200 <lianna200@gmail.com>
2017-07-08 20:42:53 -0400

    Add files via upload

---
+++ database.php
@@ -0,0 +1,13 @@
+<?p
+    $dsn = 'mysql:dbname=al224;host=sql1.njit.edu';
+    $username = 'al224';
+    $password = 'wSt5Tjc0';
+
+    try {
+        $db = new PDO($dsn, $username, $password);
+    } catch (PDOException $e) {
+        $error_message = $e->getMessage();
+        include('../errors/database_error.php');
+        exit();
+    }
+?>
